c7cac91fdcd38c2b577d0e40c1c84b15ec417f04
OwnSample <ownsample@tutanota.com>
2023-07-17 15:31:55 +0200

    Reformated bytes

--- src/bytes.h
+++ src/bytes.h
@@ -1,13 +1,13 @@
 #ifndef __BYTES__HTTP_
 #define __BYTES__HTTP_
 
-#define MB_1 1048576
-#define MB_10 10485760
-#define MB_100 104857600
-
 #define KB_1 1024
 #define KB_10 10240
 #define KB_100 102400
+
+#define MB_1 1048576
+#define MB_10 10485760
+#define MB_100 104857600
 
 #define GB_1 1073741824
 #define GB_10 10737418240
