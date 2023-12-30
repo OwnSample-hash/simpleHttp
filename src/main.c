865ce9719ca294c34af0f326cd6bd6f2083eb580
OwnSample <ownsample@tutanota.com>
2023-12-30 19:51:02 +0100

    Loggin Better

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 // simpleHttp HTTP/1.1
 
 #include "log/log.h"
+#include "lua/lua_.h"
 #include "quit_handler.h"
 #include "socket.h"
 #include "threadJob.h"
@@ -15,18 +16,19 @@ int main(int argc, char **argv) {
   signal(SIGINT, ctrl_c_h);
 
 #ifndef NO_LOG
-  log_set_level(LOG_DEBUG);
+  log_set_level(LOG_TRACE);
 #else
-  log_set_quiet(1);
+#warning "No LOG"
   log_set_level(LOG_FATAL);
+  log_set_quiet(1);
 #endif
 
-  log_debug("hello %s %s", "mario", GIT_COMMIT);
   log_info("simpleHTTP-%s HTTP/1.1", GIT_COMMIT);
-  log_info("http://127.0.0.1:%s/", (argc != 2) ? "8080" : argv[1]);
+  log_debug("http://127.0.0.1:%s/",
+            (argc != 2) ? "8080" : argv[1]); // talkin about cursed impl
   createSocket(argc, argv);
+  lua_init();
   log_info("Listening on port %d", server_port);
-  log_info("Starting to serve");
   serve();
   return 0;
 }
