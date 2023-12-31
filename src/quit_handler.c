2de9fbe56c68f5e468f024ef440258891ff5f298
OwnSample <ownsample@tutanota.com>
2023-12-31 21:19:21 +0100

    Bug fix Quit handler would go to MAX_OPEN_SOCKETS insed of the acctual lenght of the array

--- src/quit_handler.c
+++ src/quit_handler.c
@@ -8,7 +8,7 @@ int open_sockets_len = 0;
 void ctrl_c_h(int sigframe) {
   log_info("Quitting and freeing things... %d", getpid());
   lua_close(L);
-  for (int i = 0; i < MAX_OPEN_SOCKETS; i++) {
+  for (int i = 0; i < open_sockets_len; i++) {
     close(open_sockets[i].fd);
     log_debug("Closed socket %s:%d, fd: %d", open_sockets[i].conf->addr,
               open_sockets[i].conf->port, open_sockets[i].fd);
