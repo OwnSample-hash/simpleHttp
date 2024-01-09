00cdb3e7626e220d48d535937d22e92e42288424
OwnSample <ownsample@tutanota.com>
2024-01-09 22:06:08 +0100

    Reformetted

--- routes/init.lua
+++ routes/init.lua
@@ -1,21 +1,22 @@
-Add_Route('/hello', "GET", "<html><h1>Hello</h1></html>")
+Add_Route('/hello', "GET", "auto", "<html><h1>Hello</h1></html>")
 
-Add_Route("/test", "GET", function()
-	local file = io.open(SERVER_ROOT .. '/test.html', "r")
-	if not file then
-		return "<html><h1>404</h1></html>"
-	end
-	local lines = file:read("*a")
-	file:close()
-	return lines
+Add_Route("/test", "GET", "auto", function()
+  local file = io.open(SERVER_ROOT .. '/test.html', "r")
+  if not file then
+    return "<html><h1>404</h1></html>"
+  end
+  local lines = file:read("*a")
+  file:close()
+  return lines
 end)
 
-Add_Route("/lom", "GET", function()
-	return "<html><h1>" .. tostring(math.random()) .. "</h1></html>"
+Add_Route("/lom", "GET", "auto", function()
+  return "<html><h1>" .. tostring(math.random()) .. "</h1></html>"
 end)
 
-Add_Route("/scan", "GET", function()
 
+Add_Route("/_routes", "GET", "json", function()
+  return dump_table(_Funcs, 0)
 end)
 
 log(LOG_INFO, "Added routes")
