c06e0319b64cbbfba37a207c39700d95c06eab81
OwnSample <ownsample@tutanota.com>
2025-07-05 20:11:17 +0200

    define `free_stack` not just declare

--- src/stack.c
+++ src/stack.c
@@ -1,9 +1,11 @@
 #include "quit_handler.h"
+#include <stdlib.h>
 
-free_stack_t free_stack;
+free_stack_t free_stack = {0};
 
 void save_ptr(void *data) {
   if (free_stack.pos > STACK_COUNT) {
+    log_error("Stack overflow dirty exit");
     free(data);
     quick_exit(1);
   }
