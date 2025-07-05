946a9a4196691dad3162b87540e1af85927005c8
OwnSample <ownsample@tutanota.com>
2025-07-05 20:13:48 +0200

    Changed _Funcs struct

--- simpleHttpdLua/init.lua
+++ simpleHttpdLua/init.lua
@@ -5,7 +5,7 @@ if scan_ == nil or log_log == nil or _Funcs == nil or write_ == nil then
 	print("log_log:" .. tostring(log_log))
 	print("_Funcs:" .. tostring(_Funcs))
 	print("log_log:" .. tostring(write_))
-	return nil
+	os.exit(1)
 end
 
 ---Writes data to a file descriptor and returns the number of bytes written
@@ -20,29 +20,39 @@ end
 ---Route registration wrapper
 ---@param path string
 ---@param meth string
----@param cntp string Either auto or a short hand code
----@param handler function|string
+---@param cntp string Either auto or a mime type
+---@param handler function
 ---@return nil
 function Add_Route(path, meth, cntp, handler)
 	if path == "/" then
 		Warn("Adding route to path '/' will not be used TODO: fix")
-		return
+		-- return
 	end
-	if type(handler) == "string" then
-		_Funcs[path] = handler
-	else
-		if _Funcs[path] == nil then
-			_Funcs[path] = {
-				["fn"] = handler,
-				["meth"] = { meth } or { "GET" },
-				["cntp"] = cntp or "auto",
-			}
-		else
-			table.insert(_Funcs[path]["meth"], meth)
-			Info("Added meth (%s) to path (%s)", meth, path)
+	-- TODO: make fn a table
+	local index = 0
+	for i, v in ipairs(_Funcs) do
+		if v["path"] == path then
+			index = i
+			break
 		end
 	end
-	Info("Added path (%s) with meth (%s) with ~fn~ to the registry", path, meth)
+	if index == 0 then
+		index = #_Funcs + 1
+	end
+	local fn = _Funcs[index] or nil
+	if fn == nil then
+		fn = {
+			path = path,
+			meth = {
+				meth,
+			},
+			cntp = cntp,
+			handler = handler,
+		}
+		_Funcs[index] = fn
+	else
+		table.insert(fn.meth, meth)
+	end
 end
 
 LOG_TRACE = 0
@@ -62,8 +72,10 @@ function Log(lvl, fmt, ...)
 	if not caller then
 		print("NO CALLER")
 		print(debug.traceback())
+		log_log(lvl, "NO CALLER", -1, string.format(fmt, ...))
+		return
 	end
-	log_log(lvl, caller.short_src, caller.currentline, string.format(fmt, ...))
+	log_log(lvl, caller.short_src, caller.currentline, fmt:format(...))
 end
 
 --- Log functions
@@ -128,6 +140,7 @@ Info("Running additonal lua files")
 dofile("simpleHttpdLua/special_ret.lua")
 dofile("simpleHttpdLua/template.lua")
 dofile("simpleHttpdLua/autoindex.lua")
+dofile("simpleHttpdLua/static.lua")
 
 MAX_TBL_DEPTH = 5
 local tmplt1 = "{%s}"
@@ -165,10 +178,6 @@ FTW = {
 ---@param flag number
 ---@return table(string)
 function Scan(path, flag)
-	local caller = debug.getinfo(2)
-	if not caller then
-		print("NO CALLER")
-	end
 	return scan_(path, flag)
 end
 
@@ -184,7 +193,7 @@ local function table_printer(x)
 		if type(v) == "table" then
 			table_printer(v)
 		else
-			Log(LOG_DEBUG, "%s: %s", k, tostring("v"))
+			Log(LOG_DEBUG, "%s: %s", k, tostring(v))
 		end
 	end
 end
@@ -238,5 +247,6 @@ else
 	Warn("routes_scan is empty")
 end
 
-CreateAutoIndex("autoindex", "/autoindex")
+CreateAutoIndex("autoindex", "/autoindex*")
+CreateStaticServer(SERVER_ROOT, "/*")
 -- Vim: set expandtab tabstop=2 shiftwidth=2:
