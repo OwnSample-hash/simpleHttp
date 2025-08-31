795dd19734b314e34fb58a3367e64907fbc25775
OwnSample <ownsample@tutanota.com>
2025-08-31 23:22:30 +0200

    some stuff idr

--- plugins/template/src/entry.c
+++ plugins/template/src/entry.c
@@ -1,14 +1,30 @@
+#define _PLUGIN_ 1
+#include "../../../src/log/log.h"
 #include "simple_http_plugin.h"
+
+plugin_status_t event_pre_on_incoming_client(int client) {
+  log_info("Pre-processing incoming client: %d", client);
+  return HTTP_PLUGIN_OK;
+}
+
+plugin_status_t event_post_on_incoming_client(int client) {
+  log_info("Post-processing incoming client: %d", client);
+  return HTTP_PLUGIN_OK;
+}
 
 plugin_status_t plugin_init(plugin_info_t *info) {
   if (info == NULL) {
     return HTTP_PLUGIN_INVALID;
   }
-
+  info->log_log(0, __FILE__, __LINE__, "Initializing Simple HTTP Plugin");
+  info->events.event_pre_on_incoming_client = event_pre_on_incoming_client;
+  info->events.event_post_on_incoming_client = event_post_on_incoming_client;
+  info->log_log(0, __FILE__, __LINE__, "Initializing Simple HTTP Plugin");
   // Initialize the plugin information
   PLUGIN_REGISTER(info, "Simple HTTP Plugin", "1.0.0", "Author Name",
                   "A simple HTTP plugin example", PLUGIN_FLAG_NONE, 0);
   // Plugin initialization logic can go here
+  log_info("Plugin \"%s\" initialized successfully", info->name);
 
   return HTTP_PLUGIN_OK;
 }
@@ -23,4 +39,5 @@ plugin_status_t plugin_shutdown(plugin_info_t *info) {
 
   return HTTP_PLUGIN_OK;
 }
+
 // Vim: set expandtab tabstop=2 shiftwidth=2:
